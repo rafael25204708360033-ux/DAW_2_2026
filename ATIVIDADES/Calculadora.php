f4b7fd533b0cee75f8c1f1afafdd165b135295dc
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-08-10 19:18:56 -0300

    Update Calculadora.php

--- ATIVIDADES/Calculadora.php
+++ ATIVIDADES/Calculadora.php
@@ -1,1 +1,33 @@
+<?php
+$v1 = $_GET["a"];
+ $v2 = $_GET["b"];
+ $op = $_GET["op"];
 
+  if( $op == "+"):
+      $result = $v1 + $v2;
+  
+  elseif ($op == "-"):
+      $result = $v1 - $v2;
+
+  elseif ($op == "x" || $op == "*" || $op == "."):
+      $result = $v1 * $v2;
+  
+  elseif ($op == "/" || $op == ":"):
+       $result = $v1 / $v2;
+  
+  endif;
+ 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php echo "<h1>Resultado: $result</h1>"; ?>
+    
+</body>
+</html>
