770ed849d166df452556fe301551105e801a9c4a
Rafael Costa Oliveira da Conceição <rafael.25204708360033@faeterj-rio.edu.br>
2026-08-26 17:20:04 -0300

    Update ExcluirDisc.php
    Added functionality to handle POST requests and write data to disciplinas.txt.

--- ATIVIDADES/AtividadeSemana03/ExcluirDisc.php
+++ ATIVIDADES/AtividadeSemana03/ExcluirDisc.php
@@ -1,5 +1,24 @@
 <?php
     //coloquei o nome errado no arq
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')  {
+    $nome = $_POST["nome"];
+    $sigla = $_POST["sigla"];
+    $horas = $_POST["horas"];
+    $msg = "";
+    echo "nome: " . $nome . " sigla: " . " horas: " . $horas;
+   if (!file_exists("alunos.txt")) {
+       $arqDisc = fopen("disciplinas.txt","w") or die("erro ao criar arquivo");
+       $linha = "nome;sigla;horas\n";
+       fwrite($arqDisc,$linha);
+       fclose($arqDisc);
+   }
+   $arqDisc = fopen("disciplinas.txt","a") or die("erro ao criar arquivo");
+    $linha = $nome . ";" . $sigla . ";" . $horas . "\n";
+    fwrite($arqDisc,$linha);
+    fclose($arqDisc);
+    $msg = "Deu bom!";
+
+    }
 ?>
 
 <!DOCTYPE html>
